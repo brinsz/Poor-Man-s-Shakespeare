6e0b141ba0091af180a59d6898dd0f75a8585fe7
mbrinz <103621886+mbrinz@users.noreply.github.com>
2022-05-01 21:23:34 +0800

    Add files via upload

---
+++ poor_mans_poetry.py
@@ -0,0 +1,34 @@
+
+#Madlib Shakespeare's Sonnet 18
+
+print("Have a go at becoming the 21st century Shakespeare!")
+print("Fill in the prompts below and watch your poem unfold")
+
+season = input("pick a color: ")
+adj1 = input("pick an adjective with c: ")
+adj2 = input("pick an adjective with h: ")
+geographical_location = input("pick a city: ")
+mythical_location = input("pick a fictional location: ")
+precious_gem = input("your birthstone: ")
+emotion = input("your mood: ")
+zodiac = input("your zodiac sign: ")
+physics_concept =input("theoretical construct in physics or metaphysics: ")
+your_name = input("your name: ")
+
+Sonnet_18 = F"""Shall I compare thee to a {season} day?
+Thou art more {adj1} and more {adj2}.
+Rough winds do shake the darling buds of {geographical_location},
+And summer’s lease hath all too short a date.
+Sometime too hot the eye of {mythical_location} shines,
+And often is his {precious_gem} complexion dimmed;
+And every fair from fair sometime declines,
+By chance, or nature’s changing course, untrimmed;
+But thy eternal {emotion} shall not fade,
+Nor lose possession of that fair thou ow’st,
+Nor shall {zodiac} brag thou wand'rest in his shade,
+When in eternal lines to {physics_concept} thou grow'st.
+So long as men can breathe, or eyes can see,
+So long lives this, and this gives life to {your_name}."""
+
+print(Sonnet_18)
+print("I said watch it unfold, I didn't say it would be good")
